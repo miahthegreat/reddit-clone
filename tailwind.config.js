1e9b09c0d375b6c31ef90f8d4884f120e6700d3d
miahthegreat <j.j.schmid85@gmail.com>
2023-07-08 03:16:15 -0400

    removed prettier dependencies git push -u origin master

--- tailwind.config.js
+++ tailwind.config.js
@@ -88,6 +88,5 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
-    require("prettier-plugin-tailwindcss"),
   ],
 };
