26e2a324fd5c8af26b53b3166342eb5abbeaba46
miahthegreat <j.j.schmid85@gmail.com>
2023-07-10 15:53:05 -0400

    new commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className="fixed inset-x-0 top-0 z-10 h-fit border-b border-zinc-300 bg-zinc-100 py-4 dark:border-zinc-600 dark:bg-black-500">
-      <div className="container mx-auto flex h-full max-w-7xl items-center justify-between gap-6 sm:gap-4">
+    <div className="fixed inset-x-0 top-0 z-10 border-b border-zinc-300 bg-zinc-100 py-4 dark:border-zinc-600 dark:bg-black-500">
+      <div className="container mx-auto flex max-w-7xl items-center justify-between gap-6 sm:gap-4">
         {/* logo */}
         <Link href="/" className="flex items-center gap-4">
           <FcReddit className="hidden sm:block sm:h-12 sm:w-12" />
