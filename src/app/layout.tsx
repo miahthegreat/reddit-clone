0cf6af4003ded83717a856a7f2ead07504d3e8f1
miahthegreat <j.j.schmid85@gmail.com>
2023-07-10 16:04:08 -0400

    new commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Reddit Clone",
   description: "A Reddit clone built with Next.js and TypeScript.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
 };
 
 export default function RootLayout({
